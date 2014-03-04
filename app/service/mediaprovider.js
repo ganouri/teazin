b6144112561f4dfb199bc570ae86aa486c414c32
ganouri <ganouri@gmail.com>
2014-03-04 11:33:14 +1100

    media provider - optimise medalist handling

--- app/service/mediaprovider.js
+++ app/service/mediaprovider.js
@@ -10,8 +10,7 @@ define(['jquery', 'underscore', 'ev',
 
         var _this = this;
 
-        _this.localMedias = [];
-        _this.loadList = [];
+        _this.mediaList = {};
 
         function IsValidImageUrl(url,callback) {
             var img = new Image();
@@ -23,65 +22,53 @@ define(['jquery', 'underscore', 'ev',
         this.getMediaPath =  function(_id) {
             var localPath = 'file:///storage/emulated/0/Android/data/in.teaz.beta/cache/'+_id;
 
-            if (_.contains(_this.localMedias,_id)) {
+            /*if (_.contains(_this.localMedias,_id)) {
                 return localPath;
             } else {
-                if(!_.contains(_this.loadList,_id) && _id) _this.loadList.push(_id)
+                if(_id) _this.loadList.push(_id)
+                return '';
+            }*/
+            if (typeof _this.mediaList[_id] != 'undefined' && _this.mediaList[_id] = 'localMedia') {
+                return localPath;
+            } else if (_id) {
+                _this.mediaList[_id] = 'missingMedia';
                 return '';
             }
         }
 
         this.updateMediaPaths = function (order) { // asynchronous
 
-            var idList = order || _this.loadList;
+            var idList = order || _this.mediaList;
 
-            _.each(idList, function(_id){
-
-                var localPath = 'file:///storage/emulated/0/Android/data/in.teaz.beta/cache/'+_id, // ONLY FOR ANDROID
+            _.each(idList, function(type,_id){
+                if (type == 'missingMedia') {
+                    var localPath = 'file:///storage/emulated/0/Android/data/in.teaz.beta/cache/'+_id, // ONLY FOR ANDROID
                     distantPath = 'https://teazinmedias.s3.amazonaws.com/'+_id;
 
-                IsValidImageUrl(localPath, function(url, localTest) {
-                    if (localTest) {
-                        $("[mediaId='"+_id+"']").css("background-image", 'url("'+localPath+'")');
-                        _this.localMedias.push(_id);
-                        _this.loadList = _.without(_this.loadList,_id);
-
-                    } else {
-                        //IsValidImageUrl(distantPath, function(url, distantTest) {
-                            //if (distantTest) {
-                                var fileTransfer = new FileTransfer();
-
-                                fileTransfer.download(
-                                    distantPath,
-                                    localPath,
-                                    function(entry) {
-                                        $("[mediaId='"+_id+"']").css("background-image", 'url("'+localPath+'")');
-                                        _this.localMedias.push(_id);
-                                        _this.loadList = _.without(_this.loadList,_id);
-                                    },
-                                    function(error) {
-                                        console.log("download error source " + error.source);
-                                        console.log("download error target " + error.target);
-                                        console.log("upload error code" + error.code);
-                                    }
-                                );
-                            //} else {
-                                //console.log('image not found on s3'); // GUIGUI to be cleared
-                            //}
-                        //});
-                    }
-                });
-                
+                    IsValidImageUrl(localPath, function(url, localTest) {
+                        if (localTest) {
+                            $("[mediaId='"+_id+"']").css("background-image", 'url("'+localPath+'")');
+                            _this.mediaList[_id] = 'localMedia';
+                        } else {
+                            var fileTransfer = new FileTransfer();
+                            fileTransfer.download(
+                                distantPath,
+                                localPath,
+                                function(entry) {
+                                    $("[mediaId='"+_id+"']").css("background-image", 'url("'+localPath+'")');
+                                    _this.mediaList[_id] = 'localMedia';
+                                },
+                                function(error) {
+                                    console.log("download error source " + error.source);
+                                    console.log("download error target " + error.target);
+                                    console.log("upload error code" + error.code);
+                                }
+                            );
+                        }
+                    });
+                }                
             });
         };
-
-        ev.on('updateMediaPaths','MediaProvider', function(idList){
-            if (idList) {
-                this.updateMediaPaths(idList);
-            } else {
-                this.updateMediaPaths(loadList);
-            }
-        })
 
         return this;
     };
